d9f1294a7840fa6c2e0f8cc3a3e9b05672513d96
sudeshkumar2006 <sudeshkumar2003@gmail.com>
2026-06-21 13:06:50 +0530

    First commit

---
+++ First.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x=5, y=7;
+    cout<<"Hello Sudesh And Kallu Bermun\n";
+    cout<<x<<endl<<y;
+    x=9;
+    cout<<endl;
+    cout<<x;
+}
